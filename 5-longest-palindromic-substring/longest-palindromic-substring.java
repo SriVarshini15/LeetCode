ea3142307753557ee26d767581d6249cb87adfdc
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-04-18 22:13:10 +0530

    Time: 30 ms (43.72%) | Memory: 42.3 MB (74.42%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,4 +1,5 @@
 class Solution {
+/* 
     public String longestPalindrome(String s) {
         if (s == null || s.length() == 0) return "";
 
@@ -38,5 +39,50 @@ class Solution {
 
         int start = (centerIndex - maxLen) / 2;
         return s.substring(start, start + maxLen);
+    }
+
+ */
+ public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+
+        // Transform string by inserting # between characters
+        StringBuilder t = new StringBuilder();
+        t.append("#");
+        for (int i = 0; i < s.length(); i++) {
+            t.append(s.charAt(i)).append("#");
+        }
+
+        String transformed = t.toString();
+        int maxLen = 0;
+        int centerIndex = 0;
+
+        for (int i = 0; i < transformed.length(); i++) {
+            int left = i - 1;
+            int right = i + 1;
+            int currentLen = 1;
+
+            while (left >= 0 && right < transformed.length() && transformed.charAt(left) == transformed.charAt(right)) {
+                currentLen += 2;
+                left--;
+                right++;
+            }
+
+            if (currentLen > maxLen) {
+                maxLen = currentLen;
+                centerIndex = i;
+            }
+        }
+
+        // Extract original string from transformed
+        int start = (centerIndex - maxLen / 2);
+        int end = (centerIndex + maxLen / 2);
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (transformed.charAt(i) != '#') {
+                result.append(transformed.charAt(i));
+            }
+        }
+
+        return result.toString();
     }
 }
