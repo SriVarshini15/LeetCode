3adc48bdd128539d0d60c6f9148afe0a135cbdb2
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-04-27 23:29:05 +0530

    Time: 2 ms (7.16%) | Memory: 42 MB (89.30%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -4,17 +4,14 @@ class Solution {
         int i=m-1;
         int j=n-1;
         int ind=m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
                 nums1[ind--]=nums1[i--];
             }else{
                 nums1[ind--]=nums2[j--];
             }
         }
 
-        while(j>=0){
-            nums1[ind--]=nums2[j--];
-        }
         System.out.println(Arrays.toString(nums1));
     }
 }
