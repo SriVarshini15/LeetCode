13e9d7c83bfba92d3f4ec1ab5819314b23591092
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-04-29 14:46:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (61.19%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -12,4 +12,19 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
+    /* 
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = 1;
+                } else {
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+     */
 }
