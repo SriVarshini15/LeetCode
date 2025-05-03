be7219c384584bdff3edc69af1fc95e65d84d7d8
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-05-03 16:18:35 +0530

    Time: 7 ms (31.43%) | Memory: 62.8 MB (38.70%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        for(int i=0;i<nums1.length;i++){
+            if(search(nums2,nums1[i])){
+                return nums1[i];
+            }
+        }
+        return -1;
+    }
+    public static boolean search(int nums[],int target){
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(target==nums[mid]){
+                return true;
+            }if(target>nums[mid]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return false;
+    }
+}
