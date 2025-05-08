8d5f2620b955fbf8b44db6e4931b974aae21b32e
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-05-08 18:43:10 +0530

    Time: 17 ms (38.50%) | Memory: 42.9 MB (16.86%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -28,35 +28,30 @@ import java.util.HashMap;
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        // First check for null or unequal lengths
-        if (s == null || t == null || s.length() != t.length()) {
+        if(s.length()!=t.length()){
             return false;
         }
-        
-        HashMap<Character, Character> stot = new HashMap<>();
-        HashMap<Character, Character> ttos = new HashMap<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char schar = s.charAt(i);
-            char tchar = t.charAt(i);
-            
-            // Check s → t mapping
-            if (stot.containsKey(schar)) {
-                if (stot.get(schar) != tchar) {
+        HashMap<Character,Character> map1=new HashMap<>();
+        HashMap<Character,Character> map2=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char schar=s.charAt(i);
+            char tchar=t.charAt(i);
+
+            if(map1.containsKey(schar)){
+                if(map1.get(schar)!=tchar){
                     return false;
                 }
-            } else {
-                stot.put(schar, tchar);
             }
-            
-            // Check t → s mapping
-            if (ttos.containsKey(tchar)) {
-                if (ttos.get(tchar) != schar) {
+            else{
+                    map1.put(schar,tchar);
+                }
+            if(map2.containsKey(tchar)){
+                if(map2.get(tchar)!=schar){
                     return false;
                 }
-            } else {
-                ttos.put(tchar, schar);
-            }
+            }else{
+                    map2.put(tchar,schar);
+                }
         }
         return true;
     }
