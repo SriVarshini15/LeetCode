fffe21ed7590b6b80a1e5569093243b5c627d0e4
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-05-08 20:02:34 +0530

    Time: 1 ms (37.19%) | Memory: 41.2 MB (26.87%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,5 +1,6 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
+        //31 from constrain
         for(int i=0;i<31;i++){
             int check=(int) Math.pow(2,i);
             if(n==check){
