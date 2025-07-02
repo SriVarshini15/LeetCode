2a4934e974f14bfc3584d9aec3297716a86895e1
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-07-02 18:29:46 +0530

    Time: 2 ms (94.50%) | Memory: 46.6 MB (67.77%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -7,3 +7,16 @@ class Solution {
         return arr;
     }
 }
+/*
+    private int countOnes(int num) {
+    int count = 0;
+    while (num != 0) {
+        if ((num & 1) == 1) {
+            count++;
+        }
+            num >>= 1;
+        }
+        return count;
+    }
+
+ */
