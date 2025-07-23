a106e26e187d110f46212337b1ce459b75da59d1
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-07-23 21:39:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (28.18%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        list=[]
+        list.extend(nums)
+        list.extend(nums)
+        #for i in range(len(nums)*2):
+
+        return list
+
+        
