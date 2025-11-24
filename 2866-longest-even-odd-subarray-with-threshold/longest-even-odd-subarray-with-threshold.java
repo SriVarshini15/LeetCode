67d857e73734e0442ecfcd52f27d4440763aa8a9
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2025-11-24 14:16:46 +0530

    Time: 11 ms (38.21%) | Memory: 46.8 MB (25.64%) - LeetSync

---
+++ 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestAlternatingSubarray(int[] nums, int threshold) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] % 2 == 0 && nums[i] <= threshold){
+                int e=i+1;
+                while(e<n && nums[e] <= threshold && nums[e] % 2 != nums[e - 1] % 2){
+                    e++;
+                }
+                ans=Math.max(ans,e-i);
+            }
+        }
+        return ans;
+    }
+}
+
