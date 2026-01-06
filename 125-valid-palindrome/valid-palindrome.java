3c8df2a7357dd442e757c6898804e75fdc34743e
Sri Varshini <139261577+SriVarshini15@users.noreply.github.com>
2026-01-06 10:54:41 +0530

    Time: 1 ms (99.73%) | Memory: 44.1 MB (92.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,47 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str=s.toLowerCase();
-        StringBuilder sb=new StringBuilder();
+        int j=s.length()-1;
+        int i=0;
+        while(i<j){
+            char c1=s.charAt(i);
+            char c2=s.charAt(j);
+            if (c1>='A' && c1<='Z'){
+                c1=(char) (c1-'A'+'a');
+            }
+            if (c2>='A' && c2<='Z'){
+                c2=(char) (c2-'A'+'a');
+            }
+            if (checkAlpha(c1)){
+                if(checkAlpha(c2)){
+                    if (c1==c2){
+                        i++;
+                        j--;
+                        continue;
+                    }
+                    /*if(i+1==j){
+                        if(s.length()==2){
+                            if(c1!=c2){
+                                return false;
+                            }
+                        }
+                        return true;
+                    
+                    }*/
+                    return false;
+                }else{
+                    j--;
+                }
+            }else{
+                i++;
+            }
 
-        for(int i=0;i<str.length();i++){
-            if((str.charAt(i)>='a' && str.charAt(i)<='z') || (str.charAt(i)<='9' && str.charAt(i)>='0')){
-                sb.append(str.charAt(i));
-            }
-        }
-        String newStr=sb.toString();
-        //System.out.println(newStr);
-        int len=newStr.length();
-        for(int i=0;i<len/2;i++){
-            if(newStr.charAt(i)!=newStr.charAt(len-i-1)){
-                return false;
-            }
         }
         return true;
+    }
+    public static boolean checkAlpha(char c){
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c>='0' && c<='9')){
+            return true;
+        }
+        return false;
     }
 }
